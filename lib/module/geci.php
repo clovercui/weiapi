7b95231f1a756a1d3927c7277b761223166d4c8b
shanhuhai <441358019@qq.com>
2013-12-09 18:33:41 +0800

    歌词 更换接口

--- lib/module/geci.php
+++ lib/module/geci.php
@@ -1,7 +1,20 @@
 <?php
 class Module_Geci extends Module_Base{
     public function response($content = ''){
-        $content = str_replace(array(' ',' '), array('-','-'), $content);
-        return Util_OpenApi::ask('歌词 '.$content);
+        $tmp = explode(' ', $content);
+        $song = $tmp[0];
+        if(count($tmp)){
+            $paramStr = urlencode($song);
+        } else {
+            $singer = $tmp[1];
+            $paramStr = urlencode($song).'/'.urlencode($singer);
+        }
+        $url = 'http://geci.me/api/lyric/'.$paramStr;
+        $response = curl_get_content($url);
+        $response = json_decode($response, true);
+
+        $lrcUrl = $response['result'][0]['lrc'];
+        $lrcContent = curl_get_content($lrcUrl);
+        return $lrcContent;
     }
 }
